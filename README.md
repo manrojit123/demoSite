7d502d7266810d664de36999defc358b16bdea39
Rojit manandhar <manrojit123@gmail.com>
2020-02-19 14:40:33 +0545

    psl site push

--- README.md
+++
@@ -1,2 +0,0 @@
-# demoSite
-my Portfolio Demo
---
+++ script.js
@@ -0,0 +1,61 @@
+(function($) {
+  $.fn.fixMe = function() {
+    return this.each(function() {
+      var $this = $(this),
+        $t_fixed;
+      function init() {
+        $this.wrap('<div class="container" />');
+        $t_fixed = $this.clone();
+        $t_fixed
+          .find(".articlewrap")
+          .remove()
+          .end()
+          .addClass("fixed")
+          .insertBefore($this);
+        resizeFixed();
+      }
+      function resizeFixed() {
+        $t_fixed.find(".sectionhead").each(function(index) {
+          $(this).css(
+            "width",
+            $this
+              .find(".sectionhead")
+              .eq(index)
+              .outerWidth() + "px"
+          );
+        });
+      }
+      function scrollFixed() {
+        var offset = $(this).scrollTop(),
+          tableOffsetTop = $this.offset().top,
+          tableOffsetBottom =
+            tableOffsetTop +
+            $this.height() -
+            $this.find(".sectionhead").height();
+        if (offset < tableOffsetTop || offset > tableOffsetBottom)
+          $t_fixed.hide();
+        else if (
+          offset >= tableOffsetTop &&
+          offset <= tableOffsetBottom &&
+          $t_fixed.is(":hidden")
+        )
+          $t_fixed.show();
+      }
+      $(window).resize(resizeFixed);
+      $(window).scroll(scrollFixed);
+      init();
+    });
+  };
+})(jQuery);
+
+$(document).ready(function() {
+  $(".section").fixMe();
+  $(".up").click(function() {
+    $("html, body").animate(
+      {
+        scrollTop: 0
+      },
+      2000
+    );
+  });
+});
